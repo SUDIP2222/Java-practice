78850e700b9c0037b78b30a347d14a8b0d3a0b6e
Sudip <sudipdiu@gmail.com>
2019-09-22 00:49:23 +0600

    add comment

--- src/main/java/AmazonWebServiceExplore/AmazonAccessKey.java
+++ src/main/java/AmazonWebServiceExplore/AmazonAccessKey.java
@@ -2,6 +2,7 @@ package AmazonWebServiceExplore;
 
 public final class AmazonAccessKey {
 
+    //properties file spring boot
     public static final String ACCESS_KEY = "ACCESS_KEY";
     public static final String SECRET_KEY = "SECRET_KEY";
     public static final String REGION = "us-east-1";
